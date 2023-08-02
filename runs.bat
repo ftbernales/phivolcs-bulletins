a4406d3d3f4d100c6e06cf2c2ed4f1e45b680cea
Francis Jenner Bernales <64755988+ftbernales@users.noreply.github.com>
2023-08-03 07:49:49 +0800

    Batch file

---
+++ runs.bat
@@ -0,0 +1,3 @@
+python phivolcs_bulletin.py https://earthquake.phivolcs.dost.gov.ph/EQLatest-Monthly/2019/2019_February.html
+python phivolcs_bulletin.py https://earthquake.phivolcs.dost.gov.ph/EQLatest-Monthly/2019/2019_June.html
+python phivolcs_bulletin.py https://earthquake.phivolcs.dost.gov.ph/EQLatest-Monthly/2019/2019_August.html
