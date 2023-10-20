cda6fec8723b2ef48194bf1db5e6635698a08185
ftbernales <francisjenner.ae@gmail.com>
2023-10-20 09:36:54 +0800

    Add function for preprocessing combined df

--- phivolcs_bulletin_combine.py
+++ phivolcs_bulletin_combine.py
@@ -24,6 +24,35 @@ def combine_from_csv(csv_files=None, csv_dir=None):
     return df_phivolcs
     
 
+def filter_df_phivolcs(df: pd.DataFrame):
+    """
+    Returns filtered/processed dataframe using the following logic:
+
+    - Filtered out events less than M4.5
+    - 'unspecified' event types set to TECTONIC for events greater than M4.5
+    - mag_type fields set to 'Ms' for 'unspecified' event_type events less
+        than M5.5 (but greater than M4.5)
+    - mag_type fields set to 'Mw' for 'unspecified' event_type events greater
+        than or equal to M5.5
+    """
+    # Pre-process
+    df['event_type'] = df['event_type'].str.strip()
+    df['event_type'] = df['event_type'].str.replace('VOLCANO|VOLANO', 
+                                                    'VOLCANIC', regex=True)
+    # Get all data with magnitudes of at least 4.5
+    df = df[df['mag'] >= 4.5]
+    # Apply criteria on setting Ms as assumed magnitude type for unspec events
+    df.loc[(df['event_type'] == 'unspecified') & 
+           (df['mag'] < 5.5), 'mag_type'] = 'Ms'
+    # Apply criteria on setting Mw as assumed magnitude type for unspec events
+    df.loc[(df['event_type'] == 'unspecified') 
+           & (df['mag'] >= 5.5), 'mag_type'] = 'Mw'
+    # Apply criteria on setting TECTONIC as assumed event type for unspec events
+    df.loc[df['event_type'] == 'unspecified', 'event_type'] = 'TECTONIC'
+    
+    return df
+
+
 def _df_concat_from_csv_list(csv_list: list):
     df_comb = pd.DataFrame()
     for f in csv_list:
